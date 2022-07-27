90f4ce2e626fdab972054408a457fa6116ee9bb2
Cuneyt Kuscu <kuscu.cnyt@gmail.com>
2022-07-27 22:05:53 +0300

    Almira notlar

--- Basics/Basics/Program.cs
+++ Basics/Basics/Program.cs
@@ -7,41 +7,181 @@
 //var               HEPSİ
 //char              'a' 'u' '_'
 
-//5 + 9 = 14  int var
-//Almira + Cuneyt = string, var AlmiraCuneyt
-//if(9 > 10) = bit
-//10$ 150.65  = double var float
 
-//Kullanıcı Profili (İsim, Yaş ve Cinsiyet)  N adet olabilir.
+/*   1 - Girilen 2 sayının tutulduğu değişkenleri değiştir a = 10, b = 20--->a = 20, b = 10 yapacak.
 
-List<string> isimList = new List<string>();
-List<int> yasList = new List<int>();
-List<bool> cinsiyetList = new List<bool>();
 
-Console.Write("Kac Kullanici Girilecek : ");
-int profileNumber = Convert.ToInt32(Console.ReadLine());
+int numberOne, numberTwo, temp;
 
-for (int i = 0; i < profileNumber; i++)
+Console.Write("Lütfen bir Sayı Giriniz : ");
+numberOne = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Lütfen bir Sayı daha Giriniz : ");
+numberTwo = Convert.ToInt32(Console.ReadLine());
+
+temp = numberOne;
+numberOne = numberTwo;
+numberTwo = temp;
+
+Console.WriteLine("NumberOne = " + numberOne + " NumberTwo = " + numberTwo);
+
+*/
+
+/*2- Girilen Celcius sıcaklığı fahrenhiet'a çeviren program
+
+double celciusInput, fahrenheit;
+
+Console.WriteLine("Celcius Değerini Giriniz : ");
+celciusInput = Convert.ToDouble(Console.ReadLine());
+
+if (celciusInput > 500 || celciusInput < -500)
 {
-    Console.Write("İsim Listesinini " + (i + 1) + ". ismini giriniz : ");
-    isimList.Add(Console.ReadLine());
+    Console.WriteLine("Girilen Celcius Çok Büyüktür/Küçüktür.");
+}
+else
+{
+    fahrenheit = celciusInput * 2.155;
+    Console.WriteLine("Fahrenheit Değeri : " + fahrenheit);
+    Console.WriteLine("Fahreheit'ı Kelvin'e çevireyim mi ?  E/H");
 
-    Console.Write("Yaş Listesinini " + (i + 1) + ". yaşını giriniz : ");
-    yasList.Add(Convert.ToInt32(Console.ReadLine()));
-
-    Console.Write("Cinsiyet Listesinini " + (i + 1) + ". cinsiyetini giriniz : ");
-    cinsiyetList.Add( Convert.ToBoolean(Console.ReadLine()));
+    string userInput = Console.ReadLine();
+    if (userInput == "E")
+    {
+        double kelvin = 0;        
+        kelvin = fahrenheit * 32;
+        Console.WriteLine("Kelvin : "+kelvin);
+    }
+    else if (userInput == "H")
+    {
+        Console.WriteLine("Cya later bby <3");
+    }
+    else
+    {
+        Console.WriteLine("Hatalı giriş yaptınız. Şimdi en baştan dene bakalım qnq.");
+    }
 }
 
-for (int i = 0; i < profileNumber; i++)
+*/
+
+/*3 - Girilen gün sayısının ve kullanıcın seçtiği "Hafta" ya da "Ay" değerine dönüşümü 
+    135 gün, Ay -> 4 Aydır  |||| 135 gün, Hafta -> 19 hafta. 1 ay 30 gün, 1 hafta 7 gün olarak hesaplayacak.
+
+int dayCount;
+string IsWeekOrMonth;
+
+Console.WriteLine("Gun Sayisi Giriniz : ");
+dayCount = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Girilen Gün Sayısını Hafta olarak mı Yoksa Ay olarak mı İstiyorsunuz ? A/H");
+IsWeekOrMonth = Console.ReadLine();
+if(IsWeekOrMonth == "A")
 {
-    Console.WriteLine(isimList[i] + " -- " + yasList[i] + " -- " + (cinsiyetList[i] == true ? "Kadin" : "Erkek"));
+    int monthCount = dayCount / 30;
+    Console.WriteLine("Girdiğiniz Gün Sayısı "+dayCount+" gündür ve "+monthCount+" aydır.");
 }
+else if (IsWeekOrMonth == "H")
+{
+    int weekCount = dayCount / 7;
+    Console.WriteLine("Girdiğiniz Gün Sayısı " + dayCount + " gündür ve " + weekCount + " haftadır.");
+}
+else
+{
+    Console.WriteLine("Hatalı Giriş Yaptınız. BB");
+}
+
+*/
+
+/*4 - Girilen Yılın artık yıl mı yoksa normal yıl mı olduğunu yazdır
+
+int year;
+
+Console.WriteLine("Yıl değeri giriniz ");
+year = Convert.ToInt32(Console.ReadLine());
+if (year % 4 == 0)
+{
+    Console.WriteLine("Artık Yıldır");
+}
+else
+{
+    Console.WriteLine("Artık Yıl Değildir.");
+}
+*/
+
+/*5 - Girilen sayıya göre haftanın gününü yazacak, 7'den büyük girilirse, kullanıcıya mesaj gösterilip tekrar gün istenecek
+
+int dayNumber;
+
+
+soruSor:
+Console.Write("Gün Sayısı Giriniz :");
+dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (dayNumber < 8 && dayNumber > 0)
+{
+    if (dayNumber == 1) Console.WriteLine("Pazartesi");
+    if (dayNumber == 2) Console.WriteLine("Salı");
+    if (dayNumber == 3) Console.WriteLine("Çarşamba");
+    if (dayNumber == 4) Console.WriteLine("Perşembe");
+    if (dayNumber == 5) Console.WriteLine("Cuma");
+    if (dayNumber == 6) Console.WriteLine("Cumartesi");
+    if (dayNumber == 7) Console.WriteLine("Pazar");
+}
+else
+{
+    Console.WriteLine("Hatalı Giriş Yaptınız."); goto soruSor;
+}
+
+*/
+
+//6 - Girilen sayıya kadar olan çift sayıların toplamını bulan program yazılacak. 8 için 0+2+4+6 = 12 diyecek. 4 için 0+2 = 2 diyecek
+
+
+int number = 0;
+Console.WriteLine("Toplanacak Üst Limiti Giriniz : ");
+number = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+for (int i = 0; i < number; i++)
+{
+    if (i % 2 == 0)
+    {
+        sum = sum + i;
+    }
+}
+Console.WriteLine("Toplam :" + sum);
+
+
+
+Console.WriteLine("Kare boyunutunu Giriniz : ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < length; i++)
+{
+    for (int j = 0; j < length; j++)
+    {
+        Console.Write("*");
+        for (int k = 0; k < length; k++)
+        {
+            Console.Write("-");
+        }
+    }
+    Console.WriteLine();
+}
+
+
 
 
 
 
 Console.ReadKey();
+
+
+
+
+
+
+
+
 
 
 
